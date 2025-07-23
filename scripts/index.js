d46449edb38c934a226be27ba09858a71f96494c
jiyun <jyiun27@gmail.com>
2025-07-23 18:15:03 +0900

    test 제출

--- scripts/index.js
+++ scripts/index.js
@@ -1,15 +1,22 @@
 const listBox = document.querySelector('#user_list')
 const plusBtn = document.querySelector('#plus_btn')
 const closeBtn = document.querySelector('#close_btn')
+const list = document.querySelector('.list')
 const listLi = document.querySelector('.list li')
-console.log(listBox, plusBtn, closeBtn, listLi)
+const listSpan = document.querySelector('.list_check span')
+console.log(listBox, plusBtn, closeBtn, list, listLi, listSpan)
+
+list.style.display ='none'
 
 plusBtn.addEventListener('click',()=>{
     if(listBox.value == ''){
         alert('할 일을 입력하세요');
+    }else{
+        list.style.display = 'block';
     }
 })
 
 closeBtn.addEventListener('click',()=>{
     listLi.style.display = 'none'
 })
+
